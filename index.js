69590f0277b2591af30128b147c4b1df54af8d7b
sazzadwasy <sazzad.wasy@gmail.com>
2022-05-08 00:42:24 +0600

    delete inventory implementes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require('cors')
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { ObjectID } = require("bson");
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -41,6 +42,13 @@ async function run() {
         app.post('/inventory', async (req, res) => {
             const newItem = req.body
             const result = await bikeCollection.insertOne(newItem)
+            res.send(result)
+        })
+        // delete inventory
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectID(id) }
+            const result = await bikeCollection.deleteOne(query)
             res.send(result)
         })
     }
