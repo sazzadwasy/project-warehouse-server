26d0fdf405f5bad6f04ea90dc05d14708c4b2518
sazzadwasy <sazzad.wasy@gmail.com>
2022-05-08 15:13:38 +0600

    working on my items

--- index.js
+++ index.js
@@ -40,8 +40,7 @@ async function run() {
         })
         //my items
         app.get('/myitems', async (req, res) => {
-            const email = req.query.email
-            const query = { email: email }
+
             const cursor = bikeCollection.find(query)
             const items = await cursor.toArray()
             res.send(items)
